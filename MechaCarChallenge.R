45ee3e0bb9699c1abc7932ce9fc854f9f29a3411
RyanJeffery21 <quizyes@gmail.com>
2022-07-16 17:41:41 -0500

    deliverable 3 done

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -29,3 +29,18 @@ lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI
                                                                         Var_PSI=var(PSI),
                                                                         Std_Dev_PSI=sd(PSI),
                                                                         Num_Coil=n(), .groups = 'keep')
+
+# Deliverable 3
+
+#1
+t.test(coil_table$PSI,mu=1500)
+
+#2
+
+lot1<- subset(coil_table, Manufacturing_Lot=="Lot1")
+lot2<- subset(coil_table, Manufacturing_Lot=="Lot2")
+lot3<- subset(coil_table, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
